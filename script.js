4a0da3be9d5e0a842ed5c775baa85ed820258f4d
Giova0311 <povedagiova@gmail.com>
2025-04-05 17:55:47 -0500

    camibios necesarios para la presentacion

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('.section');
-    const images = document.querySelectorAll('.imagen img');
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".section");
+  const images = document.querySelectorAll(".imagen img");
 
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-            }
-        });
-    }, {
-        threshold: 0.1
-    });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
 
-    sections.forEach(section => {
-        observer.observe(section);
-    });
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
 
-    images.forEach(image => {
-        observer.observe(image);
-    });
+  images.forEach((image) => {
+    observer.observe(image);
+  });
 
+  let lastScrollY = window.scrollY;
+  const navbar = document.querySelector("nav");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > lastScrollY) {
+      navbar.style.top = "-110px";
+    } else {
+      navbar.style.top = "0";
+    }
+    lastScrollY = window.scrollY;
+  });
 });
